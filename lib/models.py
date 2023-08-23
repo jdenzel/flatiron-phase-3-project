229ad194a886c04e224b2dc4eb92b8d94d9fe4cf
Denzel <jdenzelmab@gmail.com>
2023-08-23 17:39:15 -0400

    Added an Order class

--- lib/models.py
+++ lib/models.py
@@ -16,6 +16,17 @@ class Item(Base):
     def __repr__(self):
         return f"({self.id}, {self.name}, {self.category}, {self.ingredients}, {self.price})"
 
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer(), primary_key=True)
+    item_id = Column(Integer(), ForeignKey("items.id"))
+    quantity = Column(Integer())
+    total_price = Column(Integer())
+
+    def __repr__(self):
+        return f"({self.id}, {self.item_id}, {self.quantity}, {self.total_price})"
+    
 
 
 
