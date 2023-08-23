a73641457ed577a2817a0e2b5984fb044036c30f
Denzel <jdenzelmab@gmail.com>
2023-08-23 17:38:00 -0400

    Added an Item class

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,23 @@
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    category = Column(String())
+    ingredients = Column(String())
+    price = Column(Integer())
+
+    def __repr__(self):
+        return f"({self.id}, {self.name}, {self.category}, {self.ingredients}, {self.price})"
+
+
+
+
+
+        
