78e8ed134d8535cd3aa4c1ac8e5a5fa545f5d8e6
Denzel <jdenzelmab@gmail.com>
2023-08-23 20:59:02 -0400

    Added a menu list

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
@@ -11,7 +10,8 @@ class Item(Base):
     name = Column(String())
     category = Column(String())
     ingredients = Column(String())
-    price = Column(Integer())
+    spice_level = Column(Integer())
+    price = Column(Float())
 
     def __repr__(self):
         return f"({self.id}, {self.name}, {self.category}, {self.ingredients}, {self.price})"
@@ -22,7 +22,7 @@ class Order(Base):
     id = Column(Integer(), primary_key=True)
     item_id = Column(Integer(), ForeignKey("items.id"))
     quantity = Column(Integer())
-    total_price = Column(Integer())
+    total_price = Float(Integer())
 
     def __repr__(self):
         return f"({self.id}, {self.item_id}, {self.quantity}, {self.total_price})"
